0cf95f67c391e0098cef708bb0809311c3aecce2
Haunted Chest <denyswriter@gmail.com>
2022-01-09 20:36:23 -0800

    Added the new ignisteel stuff

--- src/main/java/com/hauntedchest/the_patchlands/PatchlandsMain.java
+++ src/main/java/com/hauntedchest/the_patchlands/PatchlandsMain.java
@@ -3,10 +3,13 @@ package com.hauntedchest.the_patchlands;
 import com.hauntedchest.the_patchlands.block.ModBlocks;
 import com.hauntedchest.the_patchlands.block.block_classes.IgnisteelOreBlock;
 import com.hauntedchest.the_patchlands.item.ModItems;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
@@ -28,10 +31,17 @@ public class PatchlandsMain
         ModBlocks.register(eventBus);
 
         eventBus.addListener(this::setup);
+        eventBus.addListener(this::setupClient);
         MinecraftForge.EVENT_BUS.addListener(IgnisteelOreBlock::dropsWhenExploded);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setupClient(final FMLClientSetupEvent event) {
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.IGNISTEEL_BARS.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.IGNISTEEL_BAR_DOOR.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.IGNISTEEL_CHAINS.get(), RenderType.cutout());
     }
 
     private void setup(final FMLCommonSetupEvent event)
--- src/main/java/com/hauntedchest/the_patchlands/block/ModBlocks.java
+++ src/main/java/com/hauntedchest/the_patchlands/block/ModBlocks.java
@@ -7,7 +7,7 @@ import com.hauntedchest.the_patchlands.item.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
@@ -22,25 +22,20 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, PatchlandsMain.MOD_ID);
 
-    public static final RegistryObject<Block> BRITTLESTONE = registerBlock("brittlestone",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_GREEN)
-                    .requiresCorrectToolForDrops()
-                    .strength(3.5F, 4F)), ModCreativeTab.PATCHLANDS_BLOCKS_TAB);
+    public static final RegistryObject<Block> BRITTLESTONE = registerBlock("brittlestone", () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_GREEN).requiresCorrectToolForDrops().strength(3.5F, 8F)), ModCreativeTab.PATCHLANDS_BLOCKS_TAB);
 
-    public static final RegistryObject<Block> IGNISTEEL_ORE = registerBlock("ignisteel_ore",
-            () -> new IgnisteelOreBlock(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_YELLOW)
-                    .requiresCorrectToolForDrops()
-                    .strength(3.5F, 3F)), ModCreativeTab.PATCHLANDS_BLOCKS_TAB);
+    public static final RegistryObject<Block> IGNISTEEL_ORE = registerBlock("ignisteel_ore", () -> new IgnisteelOreBlock(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_YELLOW).requiresCorrectToolForDrops().strength(3.5F, 3F)), ModCreativeTab.PATCHLANDS_BLOCKS_TAB);
 
-    public static final RegistryObject<Block> IGNISTEEL_BLOCK = registerBlock("ignisteel_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.HEAVY_METAL, MaterialColor.COLOR_YELLOW)
-                    .requiresCorrectToolForDrops()
-                    .strength(3.5F, 3F)), ModCreativeTab.PATCHLANDS_BLOCKS_TAB);
+    public static final RegistryObject<Block> IGNISTEEL_BLOCK = registerBlock("ignisteel_block", () -> new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_YELLOW).requiresCorrectToolForDrops().strength(5F, 6F)), ModCreativeTab.PATCHLANDS_BLOCKS_TAB);
 
-    public static final RegistryObject<Block> RAW_IGNISTEEL_BLOCK = registerBlock("raw_ignisteel_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.HEAVY_METAL, MaterialColor.COLOR_YELLOW)
-                    .requiresCorrectToolForDrops()
-                    .strength(3.5F, 3F)), ModCreativeTab.PATCHLANDS_BLOCKS_TAB);
+    public static final RegistryObject<Block> RAW_IGNISTEEL_BLOCK = registerBlock("raw_ignisteel_block", () -> new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_YELLOW).requiresCorrectToolForDrops().strength(3.5F, 3.5F)), ModCreativeTab.PATCHLANDS_BLOCKS_TAB);
+
+    public static final RegistryObject<Block> IGNISTEEL_BARS = registerBlock("ignisteel_bars", () -> new IronBarsBlock(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_YELLOW).requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL).noOcclusion()), ModCreativeTab.PATCHLANDS_BLOCKS_TAB);
+
+    public static final RegistryObject<Block> IGNISTEEL_BAR_DOOR = registerBlock("ignisteel_bar_door", () -> new DoorBlock(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_YELLOW).requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL).noOcclusion()), ModCreativeTab.PATCHLANDS_BLOCKS_TAB);
+
+    public static final RegistryObject<Block> IGNISTEEL_CHAINS = registerBlock("ignisteel_chain", () -> new ChainBlock(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.NONE).requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.CHAIN).noOcclusion()), ModCreativeTab.PATCHLANDS_BLOCKS_TAB);
+
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
         RegistryObject<T> toReturn = BLOCKS.register(name,block);
